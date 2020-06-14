82eb39f4169d2cdc2ba91d612c71d870a7d59ee7
Don Puerto <don.puerto@hotmail.com>
2020-06-14 14:49:17 +0800

    Fixing Vuetify support

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,26 +1,32 @@
-// Vuetify webpack-node-externals
-const nodeExternals = require('webpack-node-externals')
+// Vuetify nodeExternals  
+const nodeExternals = require('webpack-node-externals');
 
 
 module.exports = function (api) {
-  // Vuetify webpack-node-externals
-  api.chainWebpack((config, {
-    isServer
+// Vuetify nodeExternals
+api.chainWebpack((config, { isServer }) => {
+  if (isServer) {
+    config.externals([
+      nodeExternals({
+        whitelist: [/^vuetify/, /\.css$/],
+      }),
+    ]);
+  }
+});
+
+api.loadSource(
+  ({
+    addCollection,
   }) => {
-    if (isServer) {
-      config.externals([
-        nodeExternals({
-          whitelist: [/^vuetify/]
-        })
-      ])
-    }
-  })
-
-  api.loadSource(({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+  }
+);
 
-  api.createPages(({ createPage }) => {
+api.createPages(
+  ({
+    createPage,
+  }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
+  }
+);
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
+
 import store from "../store";
 
 
@@ -12,28 +14,44 @@ export default function (Vue, {
   isClient,
   appOptions
 }) {
-     head.link.push({
-       rel: 'stylesheet',
-       href:
-         'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
-     });
+    // Vuetify setup start here...
+    head.link.push({
+      rel: 'stylesheet',
+      href:
+        'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
+    });
 
-     head.link.push({
-       rel: 'stylesheet',
-       href:
-         'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
-     });
+    head.link.push({
+      rel: 'stylesheet',
+      href:
+        'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
+    });
 
-     
+    const opts = {
+        theme: {
+        dark: false,
+        themes: {
+          dark: {
+          primary: colors.grey.darken4,
+          accent: colors.shades.black,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+          }
+        }
+      },
+      icons: {
+        iconfont: 'mdi', // default - only for display purposes
+      },
+    }; //opts includes, vuetify themes, icons, etc.
+    
+    Vue.use(Vuetify);
+    appOptions.vuetify = new Vuetify(opts);
+    // Vuetify setup end here...
 
-     // Vuetify 
-     const opts = {
-       icons: {
-         iconfont: 'mdi', // default - only for display purposes
-       },
-     }; //opts includes, vuetify themes, icons, etc.
-     Vue.use(Vuetify);
-     appOptions.vuetify = new Vuetify(opts);
+
 
      // Set Master layout as a global component
      Vue.component('Layout', MasterLayout);
